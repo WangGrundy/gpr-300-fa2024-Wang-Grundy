302af450357df5c941b587e7b25573bdcb0b4f1d
JDgoldvox <VoxcastJDgoldvox@gmail.com>
2024-02-03 15:36:08 -0500

    prepared document for ass 1)

--- assignments/assignment1/main.cpp
+++ assignments/assignment1/main.cpp
@@ -30,6 +30,10 @@ ew::Camera camera;
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 GLFWwindow* initWindow(const char* title, int width, int height);
 void drawUI();
+void RenderInMain();
+void CameraUpdate(GLFWwindow* window);
+void CameraSetUp();
+void LoadModelsAndTextures();
 
 //Global state
 int screenWidth = 1080;
@@ -37,38 +41,20 @@ int screenHeight = 720;
 float prevFrameTime;
 float deltaTime;
 
+ew::Shader shader;
+ew::Model monkeyModel;
+
 int main() {
-	GLFWwindow* window = initWindow("Assignment 0", screenWidth, screenHeight);
+	GLFWwindow* window = initWindow("Assignment 1", screenWidth, screenHeight);
 	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
 	glEnable(GL_CULL_FACE);
 	glCullFace(GL_BACK); //Back face culling
 	glEnable(GL_DEPTH_TEST); //Depth testing
 
-	//Handles to OpenGL object are unsigned integers
-	//GLuint brickTexture = ew::loadTexture("assets/materials/ground.png");
+	LoadModelsAndTextures();
 
-	GLuint tileTexture = ew::loadTexture("assets/materials/Tile/Tiles130.jpg");
-	GLuint tileNormalTexture = ew::loadTexture("assets/materials/Tile/Tiles130_Normal.png");
-
-	//load shader
-	ew::Shader shader = ew::Shader("assets/lit.vert", "assets/lit.frag");
-
-	//load model
-	ew::Model monkeyModel = ew::Model("assets/suzanne.obj");
-
-	//Bind brick texture to texture unit 0 
-	glBindTextureUnit(0, tileTexture);
-	glBindTextureUnit(1, tileNormalTexture);
-	//Make "_MainTex" sampler2D sample from the 2D texture bound to unit 0
-	shader.use();
-	shader.setInt("_MainTex", 0);
-	shader.setInt("_MainText_Normal", 1);
-
-	camera.position = glm::vec3(0.0f, 0.0f, 5.0f);
-	camera.target = glm::vec3(0.0f, 0.0f, 0.0f); //Look at the center of the scene
-	camera.aspectRatio = (float)screenWidth / screenHeight;
-	camera.fov = 60.0f; //Vertical field of view, in degrees
+	CameraSetUp();
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -77,37 +63,58 @@ int main() {
 		deltaTime = time - prevFrameTime;
 		prevFrameTime = time;
 
-		//RENDER
-		cameraController.move(window, &camera, deltaTime);
-
-		//Clears backbuffer color & depth values
-		glClearColor(0.6f, 0.8f, 0.92f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		
-		shader.use();
-		shader.setMat4("_Model", glm::mat4(1.0f));
-		shader.setMat4("_ViewProjection", camera.projectionMatrix() * camera.viewMatrix());
-		shader.setVec3("_EyePos", camera.position);
-
-		shader.setFloat("_Material.Ka", material.Ka);
-		shader.setFloat("_Material.Kd", material.Kd);
-		shader.setFloat("_Material.Ks", material.Ks);
-		shader.setFloat("_Material.Shininess", material.Shininess);
-
-		//Rotate model around Y axis
-		monkeyTransform.rotation = glm::rotate(monkeyTransform.rotation, deltaTime, glm::vec3(0.0, 1.0, 0.0));
-
-		//transform.modelMatrix() combines translation, rotation, and scale into a 4x4 model matrix
-		shader.setMat4("_Model", monkeyTransform.modelMatrix());
-
-		monkeyModel.draw(); //Draws monkey model using current shader
-
-
+		CameraUpdate(window);
+		RenderInMain();
 		drawUI();
 
 		glfwSwapBuffers(window);
 	}
 	printf("Shutting down...");
+}
+
+void RenderInMain() {
+	//Clears backbuffer color & depth values
+	glClearColor(0.6f, 0.8f, 0.92f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	shader.use();
+	shader.setMat4("_Model", glm::mat4(1.0f));
+	shader.setMat4("_ViewProjection", camera.projectionMatrix() * camera.viewMatrix());
+	shader.setVec3("_EyePos", camera.position);
+
+	shader.setFloat("_Material.Ka", material.Ka);
+	shader.setFloat("_Material.Kd", material.Kd);
+	shader.setFloat("_Material.Ks", material.Ks);
+	shader.setFloat("_Material.Shininess", material.Shininess);
+
+	//Rotate model around Y axis
+	monkeyTransform.rotation = glm::rotate(monkeyTransform.rotation, deltaTime, glm::vec3(0.0, 1.0, 0.0));
+
+	//transform.modelMatrix() combines translation, rotation, and scale into a 4x4 model matrix
+	shader.setMat4("_Model", monkeyTransform.modelMatrix());
+
+	monkeyModel.draw(); //Draws monkey model using current shader
+}
+
+void LoadModelsAndTextures() {
+
+	//Handles to OpenGL object are unsigned integers
+	//GLuint brickTexture = ew::loadTexture("assets/materials/ground.png");
+
+	GLuint tileTexture = ew::loadTexture("assets/materials/Tile/Tiles130.jpg");
+	GLuint tileNormalTexture = ew::loadTexture("assets/materials/Tile/Tiles130_Normal.png");
+
+	//load shader //load model
+	shader = ew::Shader("assets/lit.vert", "assets/lit.frag");
+	monkeyModel = ew::Model("assets/suzanne.obj");
+
+	//Bind brick texture to texture unit 0 
+	glBindTextureUnit(0, tileTexture);
+	glBindTextureUnit(1, tileNormalTexture);
+	//Make "_MainTex" sampler2D sample from the 2D texture bound to unit 0
+	shader.use();
+	shader.setInt("_MainTex", 0);
+	shader.setInt("_MainText_Normal", 1);
 }
 
 void resetCamera(ew::Camera* camera, ew::CameraController* controller) {
@@ -139,6 +146,17 @@ void drawUI() {
 
 	ImGui::Render();
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void CameraSetUp() {
+	camera.position = glm::vec3(0.0f, 0.0f, 5.0f);
+	camera.target = glm::vec3(0.0f, 0.0f, 0.0f); //Look at the center of the scene
+	camera.aspectRatio = (float)screenWidth / screenHeight;
+	camera.fov = 60.0f; //Vertical field of view, in degrees
+}
+
+void CameraUpdate(GLFWwindow* window) {
+	cameraController.move(window, &camera, deltaTime);
 }
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height)
